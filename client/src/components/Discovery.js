e8e5c6b6f8214eca807ff0ff2ead19211d9aa716
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-02 11:16:26 +0100

    Feat: Discovery with API and style

--- client/src/components/Discovery.js
+++ client/src/components/Discovery.js
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+import './Discovery.css'
 
 const API_URL_DISCOVERY = 'https://api.themoviedb.org/3/discover/tv?api_key=5237caacb7e0c70115c1cc6b132a3767&language=en-US&sort_by=popularity.desc&page=1&with_watch_monetization_types=free&with_status=0&with_type=0';
 
 const Discovery = () => {
   const [movies, setMovies] = useState([]);
+  // const history = useHistory();
 
   useEffect(() => {
     fetch(API_URL_DISCOVERY)
-    .then(response => response.json())
-    .then(data => {
-      setMovies(data.results);
-    })
-    .catch(error => {
-      console.error('Error: ', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results);
+      })
+      .catch(error => {
+        console.error('Erro ao fazer requisição:', error);
+      });
   }, []);
+
+  function handleClick(movieId) {
+    // Navegue para a página do filme
+    // history.push(`/filme/${movieId}`);
+  }
 
   return (
     <div>
-      <h1>Discovery</h1>
       <div className="movies-list">
         {movies.map(movie => (
-          <div className="movie" key={movie.id}>
-            <h2>{movie.name}</h2>
+          <div className="movie" key={movie.id} onClick={() => handleClick(movie.id)}>
+            {/* <h2>{movie.name}</h2> */}
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.name} poster`} />
           </div>
         ))}
